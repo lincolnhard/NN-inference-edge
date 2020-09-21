bc515d21196f289c339ddcbc84744881ee88e3fd
Lincoln <lincoln.lee@gallopwave.com>
2020-09-21 17:37:34 +0800

    NIE segmentation result

--- examples/bisenet.cpp
+++ examples/bisenet.cpp
@@ -20,8 +20,6 @@ static auto LOG = spdlog::stdout_color_mt("MAIN");
 
 void plotResult(cv::Mat &im, const float* tensorBuf, std::vector<int> tensorShapes, const int MAX_CLASSES)
 {
-    // TODO: ugly here
-    cv::RNG rng(12345);
     const int OUT_TENSOR_H = tensorShapes[0];
     const int OUT_TENSOR_W = tensorShapes[1];
     const int OUT_TENSOR_SIZE = OUT_TENSOR_H * OUT_TENSOR_W;
@@ -34,19 +32,62 @@ void plotResult(cv::Mat &im, const float* tensorBuf, std::vector<int> tensorShap
     cv::Mat tensorIm = cv::Mat(OUT_TENSOR_H, OUT_TENSOR_W, CV_8UC1);
     std::copy(tensorBufUint8.get(), tensorBufUint8.get() + OUT_TENSOR_SIZE, tensorIm.data);
 
+    // TODO: ugly here
+    std::vector<cv::Scalar> labelColor;
+    labelColor.push_back(cv::Scalar(0, 0, 0));      // 0: background
+    labelColor.push_back(cv::Scalar(0, 0, 255));    // 1: dashed line
+    labelColor.push_back(cv::Scalar(0, 255, 0));    // 2: solid line
+    labelColor.push_back(cv::Scalar(255, 0, 0));    // 3: pole
+    labelColor.push_back(cv::Scalar(255, 255, 0));  // 4: barrier
 
-    for (int i = 0; i < MAX_CLASSES; ++i) // 0 for background
+    for (int i = 1; i < MAX_CLASSES; ++i) // 0 for background, pass
     {
         cv::Mat currentClass = (tensorIm == i);
         if (cv::countNonZero(currentClass))
         {
             std::vector<std::vector<cv::Point>> contoursInteger;
             cv::findContours(currentClass, contoursInteger, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
-
-            cv::Scalar color = cv::Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-            cv::drawContours(im, contoursInteger, -1, color, cv::FILLED);
+            cv::drawContours(im, contoursInteger, -1, labelColor[i], cv::FILLED);
         }
     }
+}
+
+
+void outputResult(std::string outpath, const float* tensorBuf, std::vector<int> tensorShapes, const int MAX_CLASSES)
+{
+    const int OUT_TENSOR_H = tensorShapes[0];
+    const int OUT_TENSOR_W = tensorShapes[1];
+    const int OUT_TENSOR_SIZE = OUT_TENSOR_H * OUT_TENSOR_W;
+
+    auto tensorBufUint8 = std::shared_ptr<uint8_t[]>(new uint8_t[OUT_TENSOR_SIZE],
+                                    [](uint8_t *p) { delete[] p; });
+    std::transform(tensorBuf, tensorBuf + OUT_TENSOR_SIZE, tensorBufUint8.get(),
+                                    [](float const f) { return static_cast<uint8_t>(f); });
+
+    cv::Mat tensorIm = cv::Mat(OUT_TENSOR_H, OUT_TENSOR_W, CV_8UC1);
+    std::copy(tensorBufUint8.get(), tensorBufUint8.get() + OUT_TENSOR_SIZE, tensorIm.data);
+
+    cv::Mat outIm = cv::Mat(OUT_TENSOR_H, OUT_TENSOR_W, CV_8UC1, cv::Scalar(0));
+    // TODO: ugly here
+    std::vector<cv::Scalar> labels;
+    labels.push_back(cv::Scalar(0)); // 0: nie background   ->      0: nit background
+    labels.push_back(cv::Scalar(2)); // 1: nie dashed line  ->      2: nit dashed line
+    labels.push_back(cv::Scalar(1)); // 2: nie solid line   ->      1: nit solid line
+    labels.push_back(cv::Scalar(4)); // 3: nie pole         ->      4: nit pole
+    labels.push_back(cv::Scalar(3)); // 4: nie barrier      ->      3: nit barrier
+
+    for (int i = 1; i < MAX_CLASSES; ++i) // 0 for background, pass
+    {
+        cv::Mat currentClass = (tensorIm == i);
+        if (cv::countNonZero(currentClass))
+        {
+            std::vector<std::vector<cv::Point>> contoursInteger;
+            cv::findContours(currentClass, contoursInteger, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+            cv::drawContours(outIm, contoursInteger, -1, labels[i], cv::FILLED);
+        }
+    }
+
+    cv::imwrite(outpath, outIm);
 }
 
 
@@ -109,8 +150,13 @@ int main(int ac, char *av[])
             nvmodel.run();
 
             const float* scoresTensor = static_cast<const float*>(nvmodel.getHostBuffer(OUT_TENSOR_NAMES[0]));
-            plotResult(imnet, scoresTensor, OUT_TENSOR_SHAPES[0], NET_MAX_CLASSSES);
-            cv::imwrite(picpath.replace_extension().string() + "_result.jpg", imnet);
+
+            // plotResult(imnet, scoresTensor, OUT_TENSOR_SHAPES[0], NET_MAX_CLASSSES);
+            // cv::imwrite(picpath.replace_extension().string() + "_result.jpg", imnet);
+
+            std::string outpath = picpath.replace_extension().string() + ".png";
+            SLOG_INFO << outpath << std::endl;
+            outputResult(outpath, scoresTensor, OUT_TENSOR_SHAPES[0], NET_MAX_CLASSSES);
         }
     }
 
